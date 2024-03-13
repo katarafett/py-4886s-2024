72ff04e66123934c3ba5f9c6f4ae49a8bfc75908
katarafett <isfett728@gmail.com>
2024-03-12 21:31:38 -0400

    start: file struct (pt 2)

---
+++ src/main.py
@@ -0,0 +1,19 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       isfett                                                       #
+# 	Created:      3/12/2024, 9:29:12 PM                                        #
+# 	Description:  V5 project                                                   #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+
+# Brain should be defined by default
+brain=Brain()
+
+brain.screen.print("Hello V5")
+
+
+        
