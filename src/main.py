50d237055d76a1b1188653a506cb3a6d51e3fe76
katarafett <arpperry19@gmail.com>
2024-03-13 08:22:58 -0400

    vscode proj

---
+++ src/main.py
@@ -0,0 +1,19 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       aden                                                         #
+# 	Created:      3/13/2024, 8:18:46 AM                                        #
+# 	Description:  V5 project                                                   #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+
+# Brain should be defined by default
+brain=Brain()
+
+brain.screen.print("Hello V5")
+
+
+        
